c803076cea8e0846219d50575293a6f86fe47e37
Ville Inkinen <ville.inkinen@brightstraining.com>
2023-06-26 10:54:27 +0300

    Issue #3 resolved - Added function to feed the pet - Updated CMakeLists.txt - Created test_pet file for unit tests

--- includes/pet.h
+++ includes/pet.h
@@ -1,10 +1,15 @@
 #ifndef PET_H
 #define PET_H
 #include <string>
+#include <vector>
 
 struct Pet{
     std::string name;
     std::string type;
+    int happiness;
+    int fullness;
+    void feed();
 };
+void init_pet(std::vector<Pet>& all_pets);
 
 #endif //PET_H
---
+++ tests/test_pet.cpp
@@ -0,0 +1,3 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "../external/doctest.h"
+#include "../includes/pet.h"
